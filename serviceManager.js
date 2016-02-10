d5172dabbc9592bb0454b3ad8d808d93cc25e5ba
Ning Xia <nxiamail@gmail.com>
2016-02-09 21:23:38 -0800

    set up export and namespace for helper utilities

--- serviceManager.js
+++ serviceManager.js
@@ -2,3 +2,13 @@ var http    = require("http");
 var https   = require("https");
 var url     = require("url");
 var express = require("express");
+
+// module export for use in app.server.js
+module.exports = ServiceManager;
+
+// declare names for helper function classes
+var GeneralUtil, RequestUtil, DatabaseUtil;
+
+function ServiceManager(configFiles){
+    
+}
