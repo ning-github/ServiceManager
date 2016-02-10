0228fce06a74cd203143530e225854c610f035b2
Ning Xia <nxiamail@gmail.com>
2016-02-09 22:36:01 -0800

    Load config file synchronously in ConfigManager

--- configManager.js
+++ configManager.js
@@ -7,5 +7,37 @@ var +   = require("lodash");
 module.exports = ConfigManager;
 
 function ConfigManager() {
-    this.configFile = {};
+    this.configData = {};
 }
+
+ConfigManager.prototype.loadConfigFileSync = function(fileName){
+    // this hold the read JSON from the file
+    var dataString = "";
+
+    // validate that fileName is a string
+    if (!_.isString(fileName)) {
+        console.trace("ConfigManager: the fileName was not a string");
+        return null;
+    }
+
+    try {
+        // check if file exists
+        if (fs.existsSync(fileName)) {
+            // read the file into the space we declared for it
+            dataString = fs.readFileSync(fileName);
+
+            // parse it into our closure property
+            this.configData = JSON.parse(dataString);
+        }
+
+        if (_.isEmpty(this.configData)) {
+            return null;
+        } else {
+            return this.configData;
+        }
+    } catch(err) {
+        console.trace("ConfigManager: an error occurred loading config file "fileName"", err);
+    }
+
+    return null;
+};
