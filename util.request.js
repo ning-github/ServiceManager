977c010af7184ffa9e899cd2610d608c3e0aeb10
Ning Xia <nxiamail@gmail.com>
2016-02-11 21:08:13 -0800

    Create RequestUtil to abstract request handler functionality

---
+++ util.request.js
@@ -0,0 +1,13 @@
+var http       = require('http');
+var https      = require('https');
+var urlParser  = require('url');
+var path       = require('path');
+
+var _          = require('lodash');
+var Promise    = require('bluebird');
+
+module.exports = RequestUtil;
+
+function RequestUtil(options){
+
+}
