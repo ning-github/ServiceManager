4324e48af9bdc44b8359ca7ec9148cce6d3505b3
Ning Xia <nxiamail@gmail.com>
2016-02-11 21:37:23 -0800

    Create jsonRes and errorRes response handler utilities

--- util.request.js
+++ util.request.js
@@ -11,3 +11,32 @@ module.exports = RequestUtil;
 function RequestUtil(options){
 
 }
+
+// success res
+RequestUtil.prototype.jsonRes = function(res, obj, code){
+    // make sure its stringified JSON
+	var json = _.isObject(obj) ? JSON.stringify(obj) : obj;
+
+    // status code
+	if(!code) {
+        code = 200;
+    }
+
+    // successfully respond
+	res.writeHead(code, {"Content-Type": "application/json"});
+	res.end(json);
+};
+
+// error res
+RequestUtil.prototype.errorRes = function(res, errorObj, code){
+    /*
+        expects obj to come in a format:
+        {
+            key: "general.get.invalid",
+            statusCode: 400,
+            message: "this is why we can't have nice things." [optional]
+        }
+    */
+    console.error(errorObj.key);
+    return this.jsonRes(res, errorObj, errorObj.statusCode);
+};
