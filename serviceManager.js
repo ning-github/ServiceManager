ac74a8c7255b07e8e99f4897ca7979c18a2d4dd0
Ning Xia <nxiamail@gmail.com>
2016-02-14 22:14:17 -0800

    Create method to initialize services, instantiating each service and storing that instance in the manager

--- serviceManager.js
+++ serviceManager.js
@@ -2,6 +2,7 @@ var http    = require("http");
 var https   = require("https");
 var url     = require("url");
 var express = require("express");
+var Promise = require("bluebird");
 
 // module export for use in app.server.js
 module.exports = ServiceManager;
@@ -53,7 +54,7 @@ ServiceManager.prototype.setPort = function(port) {
 }
 
 ServiceManager.prototype.setRoutes = function(routes) {
-    this.routes = routes;
+    this.routesMap = routes;
 }
 
 //////////////
@@ -81,3 +82,35 @@ ServiceManager.prototype.getService = function(serviceName) {
 ServiceManager.prototype.get = function(serviceName) {
     return this.services[serviceName];
 }
+
+//////////////
+//// Initialize services
+/////////////
+ServiceManager.prototype.initializeServices = function() {
+    return new Promise(function(resolve, reject){
+        for(var serviceName in this.services) {
+            // call service constructor to initialize that service
+                // note how Service is captilized since is constructor
+                // note that the servive manager is passing itself in as the second arg
+            var service = this.services[serviceName].lib.Service(this.options, this);
+            // save itself as the service property of the existing servive object
+            this.services[serviceName].service = service;
+            /*
+                this.services of service manager instance now looks like this:
+
+                {
+                    "Search": {
+                        lib: *{library of email service}*,
+                        service: *{newly instantiated instance of the service itself}*
+                    }
+                }
+            */
+        }
+        // once initialization is complete, resolve promise so rest of setup can continue
+        resolve();
+    }.bind(this));
+};
+
+//////////////
+//// Set up the routes, using the added services
+/////////////
