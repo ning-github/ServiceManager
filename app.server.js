378a515806cb648745810f5e69787428c8e26762
Ning Xia <nxiamail@gmail.com>
2016-02-14 19:36:49 -0800

    Import service manager constructor module and search service into app server entry file

---
+++ app.server.js
@@ -0,0 +1,7 @@
+// the entry point from which we begin our server
+
+// import service manager
+var ServiceManager = require("./serviceManager.js");
+
+// import services
+var Search = require("./services/search/search.js");
