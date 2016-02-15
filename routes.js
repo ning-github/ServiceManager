fdef25d1e1bfcbd45c70bf7c7c393f60ea4cc6c6
Ning Xia <nxiamail@gmail.com>
2016-02-14 20:16:30 -0800

    Create a helloWorld route

---
+++ routes.js
@@ -0,0 +1,15 @@
+var routes = {
+    {
+        api: "/api/helloWorld",         // endpoint
+        service: "search",              // service it belongs to
+        controller: "search",           // the controller in that service
+        method: {
+            get: {                      // type of request
+                func: "getHelloWorld"   // name of controller function
+            }
+        }
+    }
+};
+
+
+module.exports = routes;
