e3fe3149b9ad91f387711e1cca1e4be2c27dc8f8
Ning Xia <nxiamail@gmail.com>
2016-02-14 22:01:39 -0800

    Capitalize Service property to indicate it is a contructor function

--- services/search/search.js
+++ services/search/search.js
@@ -2,11 +2,17 @@
 module.exports = {
 	name: "search",
     // the actual service file itself
-	service:    require("./search.service.js"),
+	Service:    require("./search.service.js"),
     // constants
 	const:      require("./search.const.js"),
     // controllers
     controller: {
         search: require("./controller/search.controller.js")
     }
+	/* when hooking up db layer, would do so through datastore modules like
+	datastore: {
+		SearchStore = require("./search.datastore.js") or something like that
+	}
+
+	*/
 };
